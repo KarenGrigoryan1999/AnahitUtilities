e7a0ff167fffd3382fd9bba58c069fd018c1c14c
KarenGrigoryan1999 <41522488+KarenGrigoryan1999@users.noreply.github.com>
2018-08-26 21:47:11 +0300

    Add files via upload

--- Uninstall Manager/WpfApp9/MainWindow.xaml.cs
+++ Uninstall Manager/WpfApp9/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace WpfApp9
         public ImageSource ImageData { get; set; }
         public string Title { get; set; }
         public string Provider { get; set; }
-        public string Size { get; set; }
     }
     class temp_item_add
     {
@@ -45,76 +44,7 @@ namespace WpfApp9
         }
         private readonly BackgroundWorker get_elements = new BackgroundWorker();
         List<string> Uninstall_paths = new List<string>();//Collection with Uninstallers paths
-        int apps_count = 0;
-        long total_size = 0;
-        void cleaning_junk_items(string app_path)
-        {
-            List<string> reg_errors = new List<string>();//Collection with registry errors
-            string[] app_paths = { };
-            if (app_path != "")
-            {
-            app_paths = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths").GetSubKeyNames();
-            foreach (var value in app_paths)
-            {
-            string[] valuenames = { };
-            string filename = "";
-            try
-            {
-            valuenames = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\" + value).GetValueNames();
-            }
-            catch{continue;}
-            finally
-            {
-            foreach (string name in valuenames)
-            {
-            try
-            {
-            filename = (string)Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\" + value).GetValue(name);
-            }
-            catch { continue; }
-            if (filename.ToLower().Contains(app_path))
-            {
-            Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths", true).DeleteValue(name);
-            reg_errors.Add(@"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths|" + name + "|Registry Key");
-            }
-            }
-            }
-            }
-            RegistryKey regkey;
-            if (Environment.Is64BitOperatingSystem)
-            regkey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.CurrentUser, RegistryView.Registry64);
-            else
-            regkey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.CurrentUser, RegistryView.Registry32);
-            app_paths = regkey.OpenSubKey(@"Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache").GetValueNames();
-            foreach (var path in app_paths)
-            {
-            if (path.ToLower().Contains(app_path))
-            {
-            regkey.OpenSubKey(@"Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache", true).DeleteValue(path);
-            reg_errors.Add(@"HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache|" + path + "|Registry Key");
-            }
-            }
-            app_paths = regkey.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store").GetValueNames();
-            foreach (var path in app_paths)
-            {
-            if (path.ToLower().Contains(app_path))
-            {
-            regkey.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store", true).DeleteValue(path);
-            reg_errors.Add(@"HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store|" + path + "|Registry Key");
-            }
-            }
-            }
-            if (reg_errors.Count > 0)
-            {
-                Window1 window = new Window1();
-                window.Show();
-                foreach (string item in reg_errors)
-                {
-                    string[] item_data = item.Split('|');
-                    window.listView.Items.Add(new temp_item_add() { Path = item_data[0], Type = item_data[2] });
-                }
-            }
-        }
+        int apps_count = 0;        
         private void get_elements_DoWork(object sender, DoWorkEventArgs e)
         {            
             void get_apps(RegistryKey OpenBaseKey)
@@ -122,15 +52,14 @@ namespace WpfApp9
                 string[] programs = OpenBaseKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall").GetSubKeyNames();
                 foreach (string app in programs)
                 {
-                    long program_size = 0;
                     string unistallstring = (string)OpenBaseKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + app).GetValue("UninstallString");
                     if (unistallstring != null || unistallstring != "")
                     {
                         try
                         {
                             RegistryKey path = OpenBaseKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + app);
-                            string installocation = (string)path.GetValue("InstallLocation");
                             string displayname = (string)path.GetValue("DisplayName");
+                            string InstallLocation = (string)path.GetValue("InstallLocation");
                             if (displayname == null || displayname == "")
                                 displayname = "Unknown";
                             string publisher = (string)path.GetValue("Publisher");
@@ -144,33 +73,13 @@ namespace WpfApp9
                             IntPtr data;
                             if (!unistallstring.ToLower().Contains("msiexec"))
                             {
-                            data = System.Drawing.Icon.ExtractAssociatedIcon(icon_path).ToBitmap().GetHbitmap();
-                                if (installocation != null && installocation != "")
-                                {
-                                DirectoryInfo program_dir = new DirectoryInfo(installocation);
-                                foreach (var directory in program_dir.GetFiles("*", SearchOption.AllDirectories))
-                                {
-                                total_size += directory.Length;
-                                program_size += directory.Length;                                       
-                                }
-                                }
+                            data = System.Drawing.Icon.ExtractAssociatedIcon(icon_path).ToBitmap().GetHbitmap(); 
                             }
                             else
                             {
                                 data = System.Drawing.Icon.ExtractAssociatedIcon(@"C:\Windows\system32\ComputerDefaults.exe").ToBitmap().GetHbitmap();
-                            }
-                            string size;                  
-                            if (program_size / 1024 / 1024 < 1024)
-                            {
-                                size = Convert.ToString(Math.Round(Convert.ToDouble((program_size+0.0) / 1024 / 1024),2)) + " Mb";
-                            }
-                            else
-                            {
-                                size = Convert.ToString(Math.Round(Convert.ToDouble((program_size + 0.0) / 1024 / 1024 / 1024),2)) + " Gb";
-                            }
-                            if (program_size == 0)
-                            size = "Unknown";
-                            App_add item = new App_add() { Title = displayname, Provider = publisher, Size = size };                 
+                            }                                              
+                            App_add item = new App_add() { Title = displayname, Provider = publisher};                 
                             object[] item_info = new object[2];
                             item_info[0] = data;
                             item_info[1] = item;
@@ -200,11 +109,6 @@ namespace WpfApp9
             item.ImageData = image;
             listView.Items.Add(item);
             programs_count.Content = apps_count+" apps were found";
-            long size = total_size / 1024 / 1024;
-            if(size<1024)
-            programs_size.Content = "Total size: " + size + " Mb";
-            else
-            programs_size.Content = "Total size: " +(size/1024) + " Gb";
         }
     void layoutRoot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -273,7 +177,6 @@ namespace WpfApp9
         private void unistall_Click(object sender, RoutedEventArgs e)
         {            
             uninstall.IsEnabled = false;
-            string app_path = "";            
             App_add selected_item = (App_add)listView.SelectedItem;
             MessageBoxResult res = MessageBox.Show("You are sure that you want to uninstall the program?", "Uninstall Manager", MessageBoxButton.YesNo, MessageBoxImage.Question);
             if (res == MessageBoxResult.Yes)
@@ -287,9 +190,8 @@ namespace WpfApp9
                     WindowStyle = ProcessWindowStyle.Hidden
                 };
                 Process.Start(process).WaitForExit();                                           
-                    Uninstall_paths.RemoveAt(listView.SelectedIndex);
-                    listView.Items.RemoveAt(listView.SelectedIndex);
-                    cleaning_junk_items(app_path);
+                Uninstall_paths.RemoveAt(listView.SelectedIndex);
+                listView.Items.RemoveAt(listView.SelectedIndex);
             }
             listView_Copy.Visibility = Visibility.Hidden;
             textBox.Text = "";
